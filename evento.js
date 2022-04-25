32cc02d2d8d025e63ee9bcac72a7206e745af8bf
DouglasBR25 <douglas2502@gmail.com>
2022-04-25 11:51:38 -0300

    first commit

---
+++ evento.js
@@ -0,0 +1,10 @@
+function clique(){
+    document.querySelector(".teste").innerHTML = "Fui clicado!";
+} 
+
+function passouMouse(){
+    document.querySelector(".teste").innerHTML = "Passou mouse!";
+}
+function tirouMouse(){
+    document.querySelector(".teste").innerHTML = "Tirou o mouse!";
+}
